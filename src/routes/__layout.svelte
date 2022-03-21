7f70b358682952377624502d14292d5f504e93f5
spences10 <spences10apps@gmail.com>
2022-03-21 12:09:27 +0000

    refactor: :recycle: use createClient in place of initClient

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
   import Footer from '$lib/components/footer.svelte'
   import Navbar from '$lib/components/navbar.svelte'
-  import { initClient } from '@urql/svelte'
+  import { createClient, setClient } from '@urql/svelte'
   import { onMount } from 'svelte'
   import { themeChange } from 'theme-change'
   import '../app.css'
 
-  initClient({
-    url: import.meta.env.VITE_GRAPHQL_API as string,
+  const client = createClient({
+    url: import.meta.env.VITE_GRAPHQL_API.toString(),
+    fetchOptions: () => {
+      return {
+        headers: {
+          authorization: `bearer ${
+            import.meta.env.VITE_SECRET_TOKEN
+          }`,
+        },
+      }
+    },
   })
+  setClient(client)
 
   onMount(() => {
     themeChange(false)
